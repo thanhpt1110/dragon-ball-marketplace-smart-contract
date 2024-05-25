f3038631df8ddc21b3041082af77ba2db81c8a88
Tuan-Thanh <thanhphantuan1110@gmail.com>
2024-05-25 16:52:45 +0700

    feat(Interaction): Check interaction with and config project Interaction by Web3 Move project to Typescript

---
+++ scripts/interactions.js
@@ -0,0 +1,136 @@
+require('dotenv').config();
+const { Web3 } = require('web3');
+const tokenAbi = [
+    {
+        "inputs": [],
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": true,
+                "internalType": "address",
+                "name": "_from",
+                "type": "address"
+            },
+            {
+                "indexed": true,
+                "internalType": "address",
+                "name": "_to",
+                "type": "address"
+            },
+            {
+                "indexed": false,
+                "internalType": "uint256",
+                "name": "_value",
+                "type": "uint256"
+            }
+        ],
+        "name": "Transfer",
+        "type": "event"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "account",
+                "type": "address"
+            }
+        ],
+        "name": "balanceOf",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "name",
+        "outputs": [
+            {
+                "internalType": "string",
+                "name": "",
+                "type": "string"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "owner",
+        "outputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "symbol",
+        "outputs": [
+            {
+                "internalType": "string",
+                "name": "",
+                "type": "string"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "totalSupply",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "to",
+                "type": "address"
+            },
+            {
+                "internalType": "uint256",
+                "name": "amount",
+                "type": "uint256"
+            }
+        ],
+        "name": "transfer",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    }
+];
+const tokenAddress = "0xDd97a27F2F2606dE83C0102FE6C396d08B2dc3E6"
+const myPrivateKey = process.env.PRIVATE_KEY;
+const myAddress = "0x3AD1B016BE50D7c169db18785AEeeB9FAFc6D4a8"
+
+async function interact() {
+    const web3 = new Web3('https://rpc.testnet.fantom.network');
+    const tokenContract = new web3.eth.Contract(tokenAbi, tokenAddress);
+
+    // Call functions from contract
+    const myBalance = await tokenContract.methods.balanceOf(myAddress).call();
+    console.log("My balance: ", myBalance);
+}
+interact();
